8b68fe0438ae4c10d0abff2b26f4522ba918ff29
Piyush7408407994 <piyushmishra7408407994@gmail.com>
2025-09-22 11:00:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var isDate = function (input) {
-  //   write your code here
+	return ! isNAN(Date.parse(input));
 };
 
 // Do not change the code below.
-const input = prompt("Enter Date.");
-alert(isDate(input));
+//const input = prompt("Enter Date.");
+//alert(isDate(input));
