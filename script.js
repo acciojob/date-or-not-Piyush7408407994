5871d2b9bd62564aa59a25cf0a59e38a39d90cfb
Piyush7408407994 <piyushmishra7408407994@gmail.com>
2025-11-10 16:17:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,5 @@
 var isDate = function (input) {
- 
-  if (input instanceof Date && !isNaN(input.getTime())) {
-    return true;
-  }
-  
-  if (typeof input === "string" || typeof input === "number") {
-    return !isNaN(Date.parse(input));
-  }
-  return false;
-};
-
-
+	return !isNaN(Date.parse(input));
+}
 const input = prompt("Enter Date.");
 alert(isDate(input));
